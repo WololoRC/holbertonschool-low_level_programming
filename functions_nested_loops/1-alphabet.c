e3a7ea79c850118b6b8502b7ea1b6c9b5817c581
WololoRC <wololoRC@proton.me>
2022-09-29 13:38:00 -0700

    task1-2

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *main -Entry point
+ *
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	int alp;
+	int nw;
+
+	nw = '\n';
+
+	alp = 97;
+	while (alp <= 122)
+	{
+		_putchar(alp);
+		_putchar(nw);
+		alp++;
+	}
+	return (void);
+}
