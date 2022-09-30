fcbe68dba4944de3977a76de1aa1d6fdb8f4f47e
WololoRC <wololoRC@proton.me>
2022-09-30 14:19:12 -0700

    task

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+/**
+ *_islower -Entry point
+ *
+ *
+ *Return: Always 0
+ *@c: variable
+ */
 int _islower(int c)
 {
-	if (c <= 122 && c >= 97 )
+	if (c <= 122 && c >= 97)
 		c = 1;
 	else
-		c = 0;	
+		c = 0;
 
 	return (c);
 }
