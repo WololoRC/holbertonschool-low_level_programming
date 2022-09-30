b2da12758a5ab803c9e4049b99f8ce99daa14372
WololoRC <wololoRC@proton.me>
2022-09-30 13:29:13 -0700

    task

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,26 @@
-#include "main.h"
+#include "_putchar.c"
+/**
+ *print_alphabet -Entry point
+ *
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+        int alp;
+        int nw;
+
+        nw = '\n';
+        alp = 97;
+
+        while (alp <= 122)
+        {
+                _putchar(alp);
+                alp++;
+
+        }
+        _putchar(nw);
+}
 /**
  *print_alphabet_x10 -Entry point
  *
