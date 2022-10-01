68ec96b34319e9a1473b61903dcc5f7ff051ca64
WololoRC <wololoRC@proton.me>
2022-10-01 11:23:57 -0700

    tsk7Jigsaw

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,36 +3,18 @@
 /**
  *print_last_digit -Entry point
  *
- *Return: Always: 1
+ *Return: Always: 0
  *@n: variable
  */
 int print_last_digit(int n)
 {
-	if (n > 10)
-	{
-		n = n % 10;
-		n = n + 48;
-	}
-
-	if (n < 10 && n != -1)
-	{
-		n = n + 48;
-	}
+	n = n % 10;
 
 	if (n < 0)
 	{
-		n = n * -1;
-		n = n % 10;
-		n = n + 48 - 2;
-
+		n /= -1;
 	}
 
-	if (n == 100)
-	{
-		n = n - 48;
-	}
-
-	_putchar(n);
-
+	_putchar(48 + n);
 	return (n);
 }
