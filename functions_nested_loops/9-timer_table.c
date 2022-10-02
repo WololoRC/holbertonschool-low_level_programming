7dca12a2aaf81e795e7b39a919bf1e290c81c12f
WololoRC <wololoRC@proton.me>
2022-10-02 15:01:38 -0700

    task9-42hours

---
+++ functions_nested_loops/9-timer_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *times_table -Function
+ *
+ */
+void times_table(void)
+{
+	int x, t, m;
+
+	for (t = 0; t <= 9; t++)
+	{
+		for (x = 0; x <= 9 ; x++)
+		{
+			m = x * t;
+
+			if ((m / 10) > 0)
+			{
+				_putchar((m / 10) + 48);
+			}
+			else
+			{
+				_putchar(' ');
+			}
+
+			_putchar((m % 10) + 48);
+
+			if (x < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
