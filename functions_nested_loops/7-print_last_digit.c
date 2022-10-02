b0ce533b33357a523aadec28b47097595bc77a32
WololoRC <wololoRC@proton.me>
2022-10-02 16:19:51 -0700

    d00mberton

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
+	n = n % 10; // ultima leta plz 
 
 	if (n < 0)
 	{
-		n /= -1;
+		n /= -1; // pasa al valor total en caso de que nos tiren num negativo
 	}
 
-	_putchar(48 + n);
+	_putchar(48 + n);  // "48" es el valor ASCII del char '0'. = 'imprimi char '0' junto con n'
 	return (n);
 }
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,25 +9,27 @@ void times_table(void)
 
 	for (t = 0; t <= 9; t++)
 	{
-		for (x = 0; x <= 9 ; x++)
+		_putchar('0');
+		_putchar(' ');
+		_putchar(',');
+
+		for (x = 1; x <= 9 ; x++)
 		{
 			m = x * t;
 
-			if ((m / 10) > 0)
+			if (((m / 10) + 48) == '0')
 			{
-				_putchar((m / 10) + 48);
+				_putchar(' ');
 			}
+			
 			else
-			{
-				_putchar(' ');
-			}
-
-			_putchar((m % 10) + 48);
-
+				_putchar((m / 10) + 48);
+				_putchar((m % 10) + 48);
+			
 			if (x < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+			{	
+			 _putchar(',');
+			 _putchar(' ');
 			}
 		}
 	_putchar('\n');
