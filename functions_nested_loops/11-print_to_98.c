2b7130efdd60d1c9e724954dd975184cbf8c0202
WololoRC <wololoRC@proton.me>
2022-10-02 18:26:42 -0700

    veryFuny

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		}
 	}
 
-	if (n > 98)
+	else if (n > 98)
 	{
 		while (n != 98)
 		{
@@ -29,7 +29,7 @@ void print_to_98(int n)
 				printf("98\n");
 		}
 	}
-	if (n == 98)
+	else if (n == 98)
 	{
 		printf("98\n");
 	}
