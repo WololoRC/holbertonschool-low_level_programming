86df0ff4a03917519ecff9829b8121165f84e02a
WololoRC <wololoRC@proton.me>
2022-10-02 18:11:19 -0700

    task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 -Function
+ *
+ *@n: input
+ *
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n != 98)
+		{
+			printf("%d, ", n);
+			n++;
+			if (n == 98)
+				printf("98 \n");
+		}
+	}
+
+	if (n > 98)
+	{
+		while (n != 98)
+		{
+			printf("%d,  ", n);
+			n--;
+			if (n == 98)
+				printf("98 \n");
+		}
+	}
+}
