75baa700e93ffe0ac47aa933cf66964272964cd8
WololoRC <wololoRC@proton.me>
2022-10-04 14:27:00 -0700

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,26 @@
 #include "main.h"
+/**
+ *print_line -print n '_' chars
+ *
+ *
+ *@n: number of line
+ */
 void print_line(int n)
 {
 	int lnt;
 
-	for (lnt = 0; lnt <= n; lnt++)
+	if (n > 0)
 	{
-		if (lnt == n)
+		for (lnt = 0; lnt <= n; lnt++)
 		{
-			_putchar('\n');
-		}
-		else
 			_putchar('_');
-	}
 
+			if (lnt == n)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+	else
+		_putchar('\n');
 }
