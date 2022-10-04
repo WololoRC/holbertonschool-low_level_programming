a0f9b2f6a3ecde5c2ddb3dd4718b777056dbf50a
WololoRC <wololoRC@proton.me>
2022-10-04 12:32:42 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,40 +1,27 @@
 #include "main.h"
 /**
- *alp -Real function
- *
- *
- */
-void alp(void)
-{
-	int x;
-
-	for (x = 0; x <= 14; x++)
-	{
-
-		if ((x + '0') > '9')
-		{
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-		}
-
-		else
-			_putchar((x + '0'));
-
-	}
-}
-/**
- *more_numbers -Im lazzy
- *
+ *more_numbers -print numbers of 0 to 14
  *
  *
  */
 void more_numbers(void)
 {
-	int y;
+	int x, y;
 
 	for (y = 0; y <= 10; y++)
 	{
-		alp();
-		_putchar('\n');
+		for (x = 0; x <= 14; x++)
+		{
+
+			if ((x + '0') > '9')
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+
+			else
+				_putchar((x + '0'));
+		}
+	_putchar('\n');
 	}
 }
