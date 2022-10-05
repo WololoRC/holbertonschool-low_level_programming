f5847c892c1649ab389e5db160bcd67828d8dfb9
WololoRC <wololoRC@proton.me>
2022-10-05 12:39:15 -0700

    task9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 			printf("Fizz ");
 		}
 
-		else if ((x % 5) == 0)
+		else if (((x % 5) == 0) && (x != 100))
 		{
 			printf("Buzz ");
 		}
@@ -30,7 +30,7 @@ int main(void)
 			printf("%d ", x);
 
 		if (x == 100)
-			printf(" \n");
+			printf("Buzz\n");
 
 	}
 
