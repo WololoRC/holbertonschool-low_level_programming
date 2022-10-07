5a54720ade44733728a458f8c76f6eade9eb2847
WololoRC <wololoRC@proton.me>
2022-10-07 14:23:11 -0700

    tsk6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *puts2 -print str sorted by 2
+ *
+ *
+ *
+ *@str: pointer
+ */
+void puts2(char *str)
+{
+	int lmt;
+
+	lmt = 0;
+
+
+	while (str[lmt] != '\0')
+	{
+		_putchar(str[lmt]);
+		lmt += 2;
+	}
+
+	_putchar('\n');
+}
