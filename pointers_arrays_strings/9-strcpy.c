b0a422c3c4c9128244e974e943e74dc492622f6c
WololoRC <wololoRC@proton.me>
2022-10-09 10:22:22 -0700

    tsk9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int len;
 
-	for (len = 0; src[len] > '\0'; len++)
+	len = 0;
+
+	while (src[len] != '\0')
 	{
 		dest[len] = src[len];
+		len++;
 	}
+
 	return (dest);
 }
