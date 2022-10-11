9dbc902c29abcfed7fbdf9ed29b5eccb573331da
WololoRC <wololoRC@proton.me>
2022-10-11 06:58:06 -0700

    tsk1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+ **_strncat -concatenates n bytes of src into dest
+ *
+ *@dest: string
+ *@src: string
+ *
+ *@n: num of bytes
+ * Return: new state of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len;
+
+	len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	while ((dest[len] = *src) >= src[n])
+	{
+		len++;
+		src++;
+	}
+
+	return (dest);
+}
