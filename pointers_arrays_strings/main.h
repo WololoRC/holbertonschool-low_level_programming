a589da0e2c4ab405a37968e5e11b41ab40c07fd9
WololoRC <wololoRC@proton.me>
2022-10-11 10:44:41 -0700

    tsk2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 
 #endif
