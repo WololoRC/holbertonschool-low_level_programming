f10c3f2b00b2e4e2ff471ed1258a2193de84d1b7
WololoRC <wololoRC@proton.me>
2022-10-13 09:01:55 -0700

    tsk2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != c && *s == '\0')
+	while (*s != c && *s != '\0')
 	{
 		s++;
 	}
 
-	if (*s != c)
+	if (*s != c && *s == '\0')
 	{
 		return (0);
 	}
