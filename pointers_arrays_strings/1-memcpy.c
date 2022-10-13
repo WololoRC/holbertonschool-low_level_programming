dfdcaf1d29e97150cc13d24c1078afd112a960c8
WololoRC <wololoRC@proton.me>
2022-10-13 06:15:54 -0700

    tsk1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,15 +9,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int fill;
+	unsigned int fill;
 
-        fill = 0;
+	fill = 0;
 
-        while (fill < n)
-        {
-                dest[fill] = src[fill];
-                fill++;
-        }
+	while (fill < n)
+	{
+		dest[fill] = src[fill];
+		fill++;
+	}
 
-        return (dest);
+	return (dest);
 }
