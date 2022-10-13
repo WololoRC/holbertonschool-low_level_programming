6648755ea27bb24893d6102ada0c9d9d1ef6297b
WololoRC <wololoRC@proton.me>
2022-10-13 07:25:24 -0700

    tsk2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ **_strchr - moves *s until find c
+ *
+ *@s: pointer on a string
+ *@c: char to find
+ *
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != c && *s != '\0')
+	{
+		*s++;
+	}
+
+	return (s);
+}
