bcb12ccf0cbb800e4208083c89ca4fefeb8f178f
WololoRC <wololoRC@proton.me>
2022-10-13 06:07:35 -0700

    tsk1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/**
+ **_memcpy - copyes n bytes *form src in *dest
+ *
+ *@src: pointer to memory to copye
+ *@dest: pointer to dest
+ *@n: NUM of bytes
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int fill;
+
+        fill = 0;
+
+        while (fill < n)
+        {
+                dest[fill] = src[fill];
+                fill++;
+        }
+
+        return (dest);
+}
