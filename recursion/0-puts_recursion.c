067df2d08d3cc4746fe26a6be1474db384945a8c
WololoRC <wololoRC@proton.me>
2022-10-14 07:59:06 -0700

    tsk0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+/**
+ *_puts_recursion - print a string followed bt a new line with recursion
+ *
+ *
+ *@s: string pointer
+ * Return: always 0
+ */
+#include "main.h"
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+
+	return (_puts_recursion(s));
+}
