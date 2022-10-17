7d9892a094363d44c45c71942f5aae3002d0c2af
WololoRC <wololoRC@proton.me>
2022-10-17 11:26:59 -0300

    tsk0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,3 +1,11 @@
+/**
+ *main -a function who says his name
+ *
+ *@argc: arg count
+ *@argv: arg vector
+ *
+ * Return: always 0
+ */
 #include <stdio.h>
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
