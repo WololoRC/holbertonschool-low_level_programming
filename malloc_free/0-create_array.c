a79e9ece1d4a8a0227cf2050867db677b95c07dd
WololoRC <wololoRC@proton.me>
2022-10-19 17:25:23 -0300

    tsk0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+/**
+ *create_array -makes and a array of a single character
+ *
+ *
+ *@size: size of the array
+ *@c: char to put in array
+ *
+ * Return: NULL or array
+ */
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	char *ar_ptr;
+	int cnt;
+
+	ar_ptr = malloc(sizeof(ar_ptr) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (ar_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (cnt = 0; cnt < size; cnt++)
+	{
+		ar_ptr[cnt] = c;
+	}
+
+	return (ar_ptr);
+
+	free(ar_ptr);
+}
