26f0794e6d3314bf348b63dc1deb1ce17af53756
WololoRC <wololoRC@proton.me>
2022-10-20 13:52:18 -0300

    tsk1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -8,10 +8,15 @@
 char *_strdup(char *str)
 {
 	int cnt, cnt1;
-	char *str_dup;
+	char *str_dup = NULL;
 
 	cnt = 0;
 	cnt1 = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
 	while (str[cnt] != '\0')
 	{
@@ -20,22 +25,16 @@ char *_strdup(char *str)
 
 	str_dup = malloc(sizeof(char) * cnt + 1);
 
+	if (str_dup == NULL)
+	{
+		return (NULL);
+	}
+
 	while (str[cnt1] != '\0')
 	{
 		str_dup[cnt1] = str[cnt1];
 		cnt1++;
 	}
 
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	else if (str_dup == NULL)
-	{
-		return (NULL);
-	}
-
-	else
-		return (str_dup);
+	return (str_dup);
 }
