4311fd48b4067cc4527bdc9a8a7a003abd8ee37c
WololoRC <wololoRC@proton.me>
2022-10-27 17:04:56 -0300

    tsk2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *print_dog -print the data of your dog
+ *
+ *
+ *@d: pointer of dog datatype
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		d->name = "(Nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(Nil)";
+	}
+
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %1.1f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
