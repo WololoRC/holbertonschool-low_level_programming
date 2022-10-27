8163c4cb66988ab201e798b9e125ba0ff2dc2f6f
WololoRC <wololoRC@proton.me>
2022-10-27 17:09:10 -0300

    tsk2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", d->name);
-		printf("Age: %1.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
 }
