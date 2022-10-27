3f8706de54989a1da05ecdb6ea1997e63920dbc5
WololoRC <wololoRC@proton.me>
2022-10-27 15:27:34 -0300

    tsk0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ *struct dog -a someones dog record data
+ *
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of the owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+#endif
