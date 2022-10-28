2569a4cf51121a22c216dc17c4ee9b4da5bc81eb
WololoRC <wololoRC@proton.me>
2022-10-28 15:20:43 -0300

    tsk5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+/**
+ *free_dog -free malloc function argument
+ *
+ *@d: dog_t pointer
+ * Return: NULL
+ */
+void free_dog(dog_t *d);
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
