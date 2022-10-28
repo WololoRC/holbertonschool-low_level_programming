51d8830aca9c0fdb3a7f9172e1c91cd8c86b89ef
WololoRC <wololoRC@proton.me>
2022-10-28 17:48:09 -0300

    tsk0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name -gives and argument to a function across a function pointer
+ *
+ *@name: argument
+ *@f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
