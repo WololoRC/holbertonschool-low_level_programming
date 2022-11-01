feb7a4cf37fd7bf5c9e80a1bdf3aeee8ac489cfa
WololoRC <wololoRC@proton.me>
2022-11-01 19:39:37 -0300

    tsk3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ *print_all - print all
+ *
+ *@format: we dont learn about switch yet
+ */
 void print_all(const char * const format, ...)
 {
-	int cnt2 = 0, cnt = 2;
+	int cnt2 = 0;
 	va_list objs;
-
-	while (format[cnt] != '\0')
-	{
-		cnt++;
-	}
 
 	va_start(objs, format);
 
@@ -18,27 +18,22 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 
-			printf("%c", va_arg(objs, int));
+			printf("%c, ", va_arg(objs, int));
 			break;
 
 			case 'i':
 
-			printf("%d", va_arg(objs, int));
+			printf("%d, ", va_arg(objs, int));
 			break;
 
 			case 'f':
 
-			printf("%f", va_arg(objs, double));
+			printf("%fi, ", va_arg(objs, double));
 			break;
 
 			case 's':
 			printf("%s", va_arg(objs, char *));
 			break;
-		}
-
-		if (cnt2 < (cnt - 1))
-		{
-			printf(", ");
 		}
 
 		cnt2++;
