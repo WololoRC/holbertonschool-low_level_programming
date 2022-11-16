11cd89c54c47f9b5311a8f8d12fb91f16b688813
WololoRC <wololoRC@proton.me>
2022-11-16 14:12:09 -0300

    tsk6

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ *pop_listint - deletes the head node of a listint_t linked list
+ *
+ *@head: ptr to head ptr
+ * Return: -1 if it fail, else returnd data of the node.
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *crt_node;
