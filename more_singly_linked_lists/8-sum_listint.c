608ea81cb8085e0c6de8f7af49ff2b3e8ec7a1e0
WololoRC <wololoRC@proton.me>
2022-11-16 15:19:23 -0300

    tsk8

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *sum_listint - sum of all the data (n) of a listint_t linked list
+ *
+ *@head: ptr to head.
+ *
+ * Return: -1 if head is NULL, @sum in succes.
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *crt_node;
+	int sum = 0;
+
+	if (!head)
+	{
+		return (-1);
+	}
+
+	crt_node = head;
+
+	while (crt_node)
+	{
+		sum += crt_node->n;
+		crt_node = crt_node->next;
+	}
+
+	return (sum);
+}
