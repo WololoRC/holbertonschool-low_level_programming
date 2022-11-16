362aeda85ab889a29a7a222e31fc1986b0ea6832
WololoRC <wololoRC@proton.me>
2022-11-16 10:47:03 -0300

    tsk4

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,9 +1,18 @@
 #include "lists.h"
+/**
+ *free_listint - free the list listint_t
+ *
+ *@head: head of the list ptr
+ */
 void free_listint(listint_t *head)
 {
+	listint_t *current_node;
+
 	while (head)
 	{
-		free(head);
+		current_node = head;
 		head = head->next;
+
+		free(current_node);
 	}
 }
