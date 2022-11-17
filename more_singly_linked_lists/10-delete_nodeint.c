796d9f2949646c0007fb42bc7892347ff34f2d07
WololoRC <wololoRC@proton.me>
2022-11-17 18:39:50 -0300

    tsk10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -12,6 +12,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *crt_node = *head;
 	listint_t *prv_node; /* prev node */
 	unsigned int cnt = 1;
+	unsigned int check = listint_len(*head);
+
+	if (index > check)
+		return (-1);
 
 	if (!*head)
 		return (-1);
@@ -37,4 +41,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	free(crt_node);
 
 	return (1);
+}
+/**
+ *listint_len - see for the lenght of a list.
+ *
+ *@h: head pointer.
+ * Return: lenght (cnt).
+ */
+size_t listint_len(const listint_t *h)
+{
+	int cnt = 0;
+
+	while (h)
+	{
+		cnt++;
+		h = h->next;
+	}
+
+	return (cnt);
 }
