2a916c9ac66539c036a7822e17bb7a944e98b088
WololoRC <wololoRC@proton.me>
2022-11-17 16:36:50 -0300

    tsk9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *insert_nodeint_at_index - insert a new node at a given position
+ *insert_nodeint_at_index - insert a new node at a given position on mid
  *
  *@head: ptr to head ptr
  *@idx: index
@@ -12,24 +12,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *crt_node, *new;
 	unsigned int cnt = 1;
 
+	new = malloc(sizeof(listint_t));
+	new->n = n;
 	if (!*head)
 	{
 		return (NULL);
 	}
-
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	crt_node = *head;
-
 	while (crt_node)
 	{
 		if (cnt >= idx)
 		{
-			new = malloc(sizeof(listint_t));
 			if (!new)
 			{
 				return (NULL);
 			}
 
-			new->n = n;
 			new->next = crt_node->next;
 			crt_node->next = new;
 
@@ -44,6 +48,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			return (NULL);
 	}
-
 	return (NULL);
 }
