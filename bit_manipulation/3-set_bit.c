e32ee74ad424bbfb25585c3e9b900a2261e6893e
WololoRC <wololoRC@proton.me>
2022-11-20 15:43:56 -0300

    tsk3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *set_bit - sets the value of a bit to 1 at a given index
+ *
+ *@n: NUM
+ *@index: bit index
+ * Return: @n modificated, -1 if fails
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bi, cnt;
+
+	if (!n)
+		return (-1);
+
+	bi = 1;
+	cnt = 1;
+
+	while (cnt <= index)
+	{
+		bi *= 2;
+		cnt++;
+	}
+
+	*n += bi;
+
+	return (1);
+}
