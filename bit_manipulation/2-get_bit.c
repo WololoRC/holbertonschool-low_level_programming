ea331e112858d54ba776e6c5539d553610a9fad4
WololoRC <wololoRC@proton.me>
2022-11-19 21:11:25 -0300

    tsk2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *get_bit - returns the value of a bit at a given index
+ *
+ *@n: NUM
+ *@index: S/T
+ * Return: value of bit on succes, -1 if fails.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int cnt, in_bit, ret;
+
+	cnt = 0;
+	in_bit = 1;
+
+	if (!n)
+		return (-1);
+
+	while (cnt <= index)
+	{
+		in_bit *= 2;
+		cnt++;
+	}
+
+	in_bit /= 2;
+
+	if (n & in_bit)
+		ret = 1;
+
+	else
+		ret = 0;
+
+	return (ret);
+}
