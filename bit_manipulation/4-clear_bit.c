0735929dc875964b09fe2d1b6220882ae219dc1b
WololoRC <wololoRC@proton.me>
2022-11-20 16:30:39 -0300

    tsk4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *celar_bit - sets the value of a bit to 0 at a given index
+ *clear_bit - sets the value of a bit to 0 at a given index
  *
  *@n: NUM
  *@index: bit
@@ -9,8 +9,6 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int bi = 1;
-
 	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
