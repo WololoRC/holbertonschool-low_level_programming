00977c0f2720935652c9da5d9bad666d9e5fce5e
WololoRC <wololoRC@proton.me>
2022-11-22 12:07:47 -0300

    tsk1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, cnt;
+	int fd, wr, cnt;
 
 	cnt = 0;
 
@@ -21,7 +21,9 @@ int create_file(const char *filename, char *text_content)
 		cnt++;
 	}
 
-	write(fd, text_content, cnt);
+	wr = write(fd, text_content, cnt);
+	if (wr < -1)
+		return (-1);
 
 	close(fd);
 	return (1);
