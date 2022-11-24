99eaa1cda7eefb95827bef8817b97a8c50efbdc7
WololoRC <wololoRC@proton.me>
2022-11-24 16:29:45 -0300

    tsk7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -51,4 +51,29 @@ size_t dlistint_len(const dlistint_t *h)
 
         return (cnt);
 }
+#include "lists.h"
+/**
+ *get_dnodeint_at_index - get a nth node from a dlistint_t list
+ *
+ *@head: node ptr
+ *@index: nth node position
+ * Return: nth node on success, NULL if it fails.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+        unsigned int cnt = 0;
+
+        while (cnt < index)
+        {
+                head = head->next;
+                if (!head)
+                {
+                        return (NULL);
+                }
+                cnt++;
+        }
+
+        return (head);
+}
+
 
