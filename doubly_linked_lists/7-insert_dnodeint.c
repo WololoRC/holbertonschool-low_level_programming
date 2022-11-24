05087ccf88389f34e03d5b4c095c6366aee57d16
WololoRC <wololoRC@proton.me>
2022-11-24 16:27:22 -0300

    tsk7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,60 +10,45 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *crt_node, *new;
-	unsigned int cnt = 1;
 	unsigned int check = dlistint_len(*h);
 
 	if (idx > check)
 		return (NULL);
 
-	crt_node = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	crt_node = get_dnodeint_at_index(*h, (idx - 1));
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
 
-	if (idx == 0)
-	{
-		new->n = n;
-		new->next = *h;
-		new->prev = NULL;
-		*h = new;
-		return (new);
-	}
+	new->n = n;
+	new->next = crt_node->next;
+	crt_node->next = new;
+	new->prev = crt_node;
 
-	while (cnt <= idx)
-	{
-		if (cnt == idx)
-		{
-			new->n = n;
-			new->next = crt_node->next;
-			crt_node->next = new;
-			new->prev = crt_node;
-			new->next->prev = new;
-		}
-
-		crt_node = crt_node->next;
-		cnt++;
-
-	}
+	if ((new->next))
+		new->next->prev = new;
 
 	return (new);
 }
+#include "lists.h"
 /**
- *dlistint_len - see for the lenght of a dlistint_t list.
+ *dlistint_len - count the number of elements on dlistint_t list
  *
- *@h: head pointer.
- * Return: lenght (cnt).
+ *@h: ptr to head
+ * Return: cnt.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int cnt = 0;
+        unsigned int cnt = 0;
 
-	while (h)
-	{
-		cnt++;
-		h = h->next;
-	}
+        while (h)
+        {
+                cnt++;
+                h = h->next;
+        }
 
-	return (cnt);
+        return (cnt);
 }
+
