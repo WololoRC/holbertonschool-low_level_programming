a510a388812afd0761415742734182919148717f
WololoRC <wololoRC@proton.me>
2022-11-25 17:07:07 -0300

    tsk3

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  *@size: size of hash table
  * Return: @new_key (djb2 value of @key % size).
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const char *key, unsigned long int size)
 {
 	unsigned long int new_key = hash_djb2(key);
 
--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,6 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL;
 
 	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
 
 	new->key = strdup(key);
 	new->value = strdup(value);
