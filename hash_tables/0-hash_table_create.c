7b6f14c1ccdf0b708ddfba12ece5f036ea28fd25
WololoRC <wololoRC@proton.me>
2022-11-25 14:08:35 -0300

    tsk0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - creates a hash table.
+ *
+ *@size: size of
+ *
+ * Return: @new hash table on success, NULL if it fails.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+
+	new = malloc(sizeof(hash_table_t));
+	if(!new)
+		return (NULL);
+
+	new->array = malloc(sizeof(hash_node_t) * size);
+	if (!new->array)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->size = size;
+
+	return(new);
+}
