e9743d7580880f6b8e4d789d27e612834e530c38
WololoRC <wololoRC@proton.me>
2022-11-25 17:51:45 -0300

    tsk3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,18 +9,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, sizeof(*ht));
+	unsigned long int index = key_index(key, ht->size);
 	hash_node_t *new = NULL;
 
-	new = malloc(sizeof(hash_node_t));
+	new = add_node(&new, (const char *) key, (const char *) value);
 	if (!new)
 		return (0);
 
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
-
-	ht->array[index] = new;
+	if (!ht->array[index])
+	{
+		ht->array[index] = new;
+		return (1);
+	}
 
 	return (1);
+}
+/**
+ *add_node - add nodes at the beginning of a hash_node_t list
+ *
+ *@head: head pointer
+ *@value: value
+ *@key: key
+ * Return: if !malloc returns NULL, else a new node.
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (NULL);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
