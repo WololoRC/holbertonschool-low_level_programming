a32f3e26bff1ea5c69604663d43e11ffa1995758
WololoRC <wololoRC@proton.me>
2022-11-26 16:36:12 -0300

    looking for trouble

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,24 @@
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp = NULL;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			printf("'%s' : '%s'", temp->key, temp->value);
+			if (ht->array[i + 1] == NULL)
+				break;
+			printf(", ");
+			temp = temp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
