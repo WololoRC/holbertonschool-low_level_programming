f940134e46ef4d4da8b28fb574f81be50f6e88b7
WololoRC <wololoRC@proton.me>
2023-01-05 11:27:53 -0300

    tsk0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - founda a value on an array by linear searching
+ *
+ *@array: array
+ *@size: array size
+ *@value: value to search
+ * Return: value, and value position on success, -1 if fails.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int cnt = 0;
+
+	while (cnt <= (size - 1))
+	{
+		printf("Value checked array[%d] = [%d]\n", cnt, array[cnt]);
+		if (array[cnt] == value)
+		{
+			return (cnt);
+		}
+
+		cnt++;
+	}
+
+	return (-1);
+}
